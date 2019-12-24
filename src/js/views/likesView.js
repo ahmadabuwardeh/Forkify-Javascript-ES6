a7ff11ae36cd4803ad46735b0eaa7e8c9a5c26ab
Ahmad Abuwardeh <ahmadabuwardeh@users.noreply.github.com>
2019-12-24 17:09:41 +0200

    Final

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -6,7 +6,6 @@ export const toggleLikeBtn = isLiked => {
   document
     .querySelector(".recipe__love use")
     .setAttribute("href", `img/icons.svg#${iconString}`);
-  
 };
 
 export const toggleLikeMenu = numLikes => {
